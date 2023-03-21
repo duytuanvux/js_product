8cf9e1c60235b0cfb220fd243c4c145b5b9cb748
Vu Duy Tuan <contact.duytuan@gmail.com>
2023-03-21 23:59:18 +0700

    lab-2

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // Load lại thành phần vào trang chủ
+    $('#nav').load('components/menu.html')
+    $('#slide').load('components/slide.html')
+    $('#banner').load('components/banner.html')
+    $('#product-list').load('components/products.html')
+})
